88c35738511edcacc23c38bb3b999efd19728cf0
JaeOhHa <gkwodhsla@naver.com>
2019-10-10 23:02:03 +0900

    commit3
    배경 스크롤링을 구현함.

--- background.py
+++ background.py
@@ -1,16 +1,42 @@
 from pico2d import *
 import random
 
-windowWIDTH=800
-windowHEIGHT=600
+windowWIDTH = 800
+windowHEIGHT = 600
 
-open_canvas(windowWIDTH,windowHEIGHT)
-#background=load_image("background\\"+str(random.randint(1,5))+".png")
-background=load_image("background\\1.png")
+open_canvas(windowWIDTH, windowHEIGHT)
+
+background = load_image("background\\" + str(random.randint(1, 5)) + ".png")
+
 x = 0
 y = 0
 
 cameraXCoord = 0
 
+moveRight = False
+
+moveLeft = False
+
+
 def draw():
-    background.clip_draw_to_origin(x,0,windowWIDTH,windowHEIGHT,0,0)
+    global cameraXCoord
+    background.clip_draw_to_origin(x + cameraXCoord, 0, windowWIDTH, windowHEIGHT, 0, 0)
+
+
+def handleEvent(mouseX):
+    global moveLeft, moveRight
+    if 0 < mouseX < 150:
+        moveLeft = True
+    elif windowWIDTH - 150 < mouseX < windowWIDTH:
+        moveRight = True
+    else:
+        moveLeft = False
+        moveRight = False
+
+
+def update():
+    global cameraXCoord
+    if moveRight and cameraXCoord < background.w - windowWIDTH:
+        cameraXCoord += 2
+    elif moveLeft and cameraXCoord > 0:
+        cameraXCoord -= 2
--- gameLoop.py
+++ gameLoop.py
@@ -1,19 +1,24 @@
 import background
 from pico2d import *
 
+
 def eventLoop():
     global running
-    events=get_events()
+    events = get_events()
     for event in events:
-        if event.type==SDL_QUIT:
+        if event.type == SDL_QUIT:
             running = False
-
+        elif event.type == SDL_MOUSEMOTION:
+            background.handleEvent(event.x)
 
 
 running = True
 
 while running:
     eventLoop()
+
+    background.update()
+
     clear_canvas()
     background.draw()
     update_canvas()
