b51e4b7d78bd6c258be2c88e2e766e95244b8f80
JaeOhHa <gkwodhsla@naver.com>
2019-10-17 21:20:13 +0900

    commit 07
    class 의 네이밍을 수정함.

--- allyCharacter.py
+++ allyCharacter.py
@@ -2,29 +2,29 @@ from characterAttrib import *
 
 loadKnightImage()
 
-class knight1(characterABC):
-    state=characterState.WALK
+class Knight1(CharacterABC):
+    state=CharacterState.WALK
     def __init__(self,x,y):
         self.x=x
         self.y=y
         self.frame=0
     def draw(self):
-        if(self.state==characterState.WALK):
+        if(self.state==CharacterState.WALK):
             walkImageList[self.frame%len(walkImageList)].draw(self.x-camera.cameraXCoord,self.y,self.size,self.size)
-        elif(self.state==characterState.IDLE):
+        elif(self.state==CharacterState.IDLE):
             walkImageList[self.frame % len(walkImageList)].draw(self.x - camera.cameraXCoord, self.y, self.size,self.size)
 
     def move(self):
         pass
 
     def update(self):
-        if(self.state==characterState.WALK):
+        if(self.state==CharacterState.WALK):
             self.frame+=1
             self.x+=0.1
 
     def checkCollision(self,frontCharacterXpos):
         if(self.x+self.size>frontCharacterXpos):
-            self.state=characterState.IDLE
+            self.state=CharacterState.IDLE
 
     def changeState(self):
         pass
--- characterAttrib.py
+++ characterAttrib.py
@@ -1,24 +1,26 @@
 from abc import *
 from enum import Enum
 import camera
-from pico2d import*
+from pico2d import *
 
-walkImageList=[]
+walkImageList = []
+
 
 def loadKnightImage():
     for i in range(0, 6 + 1):
         walkImageList.append(load_image("Ally\\Knight\\1_KNIGHT\\_WALK\\_WALK_00" + str(i) + ".png"))
 
 
+class CharacterState(Enum):
+    IDLE = 0,
+    ATTACK = 1,
+    WALK = 2,
+    DIE = 3
 
-class characterState(Enum):
-    IDLE=0,
-    ATTACK=1,
-    WALK=2,
-    DIE=3
 
-class characterABC(metaclass=ABCMeta):
-    size=100
+class CharacterABC(metaclass=ABCMeta):
+    size = 100
+
     def draw(self):
         pass
 
@@ -29,10 +31,10 @@ class characterABC(metaclass=ABCMeta):
         pass
 
     def checkCollision(self):
-        #충돌처리는 월드 오브젝트 리스트의 맨앞에 있는 애들은 적군을 만났는지 아닌지 충돌체크하고 만나면 상태를 ATTACK으로 바꾼다.
-        #나머지뒤에 오브젝트들은 앞에 아군과 충돌인지 아닌지 검사한다. 만일 아군과 충돌이면 상태를 IDLE하게 해준다.
-        #아군 self.x+self.size<frontobject's Xpos
-        #적군 self.xpos>frontobject's Xpos + self.size로 충돌체크해주면됨.
+        # 충돌처리는 월드 오브젝트 리스트의 맨앞에 있는 애들은 적군을 만났는지 아닌지 충돌체크하고 만나면 상태를 ATTACK으로 바꾼다.
+        # 나머지뒤에 오브젝트들은 앞에 아군과 충돌인지 아닌지 검사한다. 만일 아군과 충돌이면 상태를 IDLE하게 해준다.
+        # 아군 self.x+self.size<frontobject's Xpos
+        # 적군 self.xpos>frontobject's Xpos + self.size로 충돌체크해주면됨.
         pass
 
     def changeState(self):
